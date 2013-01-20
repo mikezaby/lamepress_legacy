bc610730c175a529e69095ef9926bed2ab4700a1
mikezaby <mikezaby@gmail.com>
2013-01-20 22:38:51 +0200

    fetch id on before filter

--- app/controllers/admin/article_controller.rb
+++ app/controllers/admin/article_controller.rb
@@ -4,10 +4,13 @@ class Admin::ArticleController < Admin::BaseController
 
   ActionController::Base.prepend_view_path("app/themes/#{$layout}")
 
+  before_filter :assign_search, only: [:index, :search]
+  before_filter :fetch_article, only: [:edit, :update, :destroy, :show]
+
   def index
-    @q = Article.includes(:issue, :category).search(params[:q], :auth_object => 'admin')
     @article = Article.issued.page(params[:page]).order("date DESC").per(20)
-    @nonis_article = Article.non_issued.page(params[:ni_page]).order("date DESC").per(20)
+    @nonis_article = Article.non_issued.page(params[:ni_page]).
+                             order("date DESC").per(20)
   end
 
   def new
@@ -15,8 +18,8 @@ class Admin::ArticleController < Admin::BaseController
   end
 
   def search
-    @q = Article.includes(:issue, :category).search(params[:q], :auth_object => 'admin')
-    @article = @q.result(:distinct => true).page(params[:page]).order("date DESC").per(20)
+    @article = @q.result(distinct: true).order("date DESC").
+                  page(params[:page]).per(20)
   end
 
   def create
@@ -33,11 +36,9 @@ class Admin::ArticleController < Admin::BaseController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if params[:article][:preview] == "1" && @article.valid?
       show
     elsif @article.update_attributes(params[:article])
@@ -48,37 +49,22 @@ class Admin::ArticleController < Admin::BaseController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to(admin_articles_path)
   end
 
   def show
-    @article = Article.find(params[:id]) if @article.nil?
     @issue = @article.issue ||  Setting.current_issue || Issue.first
-    @category = @article.category
     render "/article/issued_article", layout: $layout
   end
 
+  private
 
-  def sitemap
-    @articles = Article.includes(:issue, :category).where(published: true).order("created_at DESC")
-    File.delete("#{Rails.root}/public/sitemap.xml") if File.exists?("#{Rails.root}/public/sitemap.xml")
-
-    sitemap = File.new("#{Rails.root}/public/sitemap.xml", "w")
-    sitemap.puts('<?xml version="1.0" encoding="utf-8"?>')
-    sitemap.puts('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
-    @articles.each do |article|
-      sitemap.puts('<url>')
-      url = article.issue_id.nil? ?  "/#{article.category_permalink}/#{article.id}-#{article.prettify_permalink}" : "/issue_#{article.issue_number}/#{article.category_permalink}/#{article.id}-#{article.prettify_permalink}"
-      sitemap.puts("<loc>#{$domain}#{url}/</loc>")
-      sitemap.puts("<lastmod>#{article.updated_at.strftime("%Y-%m-%d")}</lastmod>")
-      sitemap.puts('</url>')
-    end
-    sitemap.puts('</urlset>')
-    sitemap.close
-    redirect_to admin_articles_url, notice: 'Sitemap was successfully created.'
+  def assign_search
+    @q = Article.includes(:issue, :category).search(params[:q], auth_object: 'admin')
   end
 
+  def fetch_article
+    @article = @article = Article.find(params[:id])
+  end
 end
-
