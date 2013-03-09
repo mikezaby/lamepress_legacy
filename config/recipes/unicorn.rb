65bb6a4c57d26e56d5ce2e7bab2fefbfb1c1fe1a
mikezaby <mikezaby@gmail.com>
2013-03-09 17:21:00 +0200

    access unicorn service by path

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -19,7 +19,7 @@ namespace :unicorn do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn"
     task command, roles: :app do
-      run "service unicorn_#{application} #{command}"
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
     after "deploy:#{command}", "unicorn:#{command}"
   end
