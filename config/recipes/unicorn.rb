9affcb853009d227a0e6bc05a32b4489fd44dc6e
mikezaby <mikezaby@gmail.com>
2013-03-09 17:14:19 +0200

    remove sudo on unicorn restart

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -19,7 +19,7 @@ namespace :unicorn do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn"
     task command, roles: :app do
-      run "#{sudo} service unicorn_#{application} #{command}"
+      run "service unicorn_#{application} #{command}"
     end
     after "deploy:#{command}", "unicorn:#{command}"
   end
