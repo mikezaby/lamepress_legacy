298e7a21b0e26d1f2ce5fbd4a6d6b16524448878
mikezaby <mikezaby@gmail.com>
2013-10-12 19:14:35 +0300

    Reorganize code at category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,8 @@
 class Category < ActiveRecord::Base
 
-  # for using 'truncate' method on prettify_permalink
   include ActionView::Helpers::TextHelper
 
-  before_save :prettify_permalink
+  ORDER_ARTICLES = { desc: 0, asc: 1 }
 
   has_many :articles
   has_many :navigators, as: :navigatable, dependent: :destroy
@@ -11,17 +10,17 @@ class Category < ActiveRecord::Base
   validates :name, presence: true
   validates :permalink, uniqueness: true
 
+  before_save :prettify_permalink
+
   attr_accessible :name, :permalink, :issued, :order_articles, :mode
-
-  ORDER_ARTICLES = { desc: 0, asc: 1 }
-
-  def ordered
-    ORDER_ARTICLES.keys[order_articles]
-  end
 
   scope :mobile_issued, -> { where(issued: true).select("id, name, permalink") }
   scope :issued, -> { where(issued: true) }
   scope :non_issued, ->  { where(issued: false) }
+
+  def ordered
+    ORDER_ARTICLES.keys[order_articles]
+  end
 
   private
 
