740da25a4c24a1725fba68c91d9d09546bcb07a6
mikezaby <mikezaby@gmail.com>
2013-10-12 19:15:36 +0300

    Reorganize code at ordering model

--- app/models/ordering.rb
+++ app/models/ordering.rb
@@ -1,8 +1,8 @@
 class Ordering < ActiveRecord::Base
   belongs_to :article, touch: true
 
+  delegate :title, :issue_id, to: :article, prefix: true
+
   scope :issue, -> { where('issue_pos IS NOT NULL').order("issue_pos") }
   scope :category, -> { order("cat_pos") }
-
-  delegate :title, :issue_id, to: :article, prefix: true
 end
