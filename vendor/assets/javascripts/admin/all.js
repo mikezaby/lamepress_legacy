84fa155552f8cb6e12e0c303227463e58aec4c06
mikezaby <mikezaby@gmail.com>
2015-03-10 00:27:01 +0200

    Add handlebars runtime and templates folder to admin.js

--- vendor/assets/javascripts/admin/all.js
+++ vendor/assets/javascripts/admin/all.js
@@ -4,6 +4,8 @@
 //= require admin/bootstrap.min
 //= require admin/metisMenu.min
 //= require ckeditor/init
+//= require handlebars.runtime
 //= require admin/sb-admin-2
 //= require admin/sortable
 //= require core/category_selector
+//= require_tree ../core/templates
