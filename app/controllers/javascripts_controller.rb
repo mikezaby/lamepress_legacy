05617021f0ffb187b7c8ba257724a7118df3430e
mikezaby <mikezaby@gmail.com>
2015-03-10 00:27:01 +0200

    Remove JavascriptContronller#dynamic_dates

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -1,13 +1,4 @@
 class JavascriptsController < ApplicationController
-  def dynamic_dates
-    @issues = Issue.find(:all, :order => "number DESC")
-  end
-
-  def dynamic_dates_pub
-    @issues = Issue.where(published: true).order("number DESC")
-    render "dynamic_dates"
-  end
-
   def date_picker
 
   end
